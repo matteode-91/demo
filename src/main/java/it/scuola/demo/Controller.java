2f7919e093a25cb5e2a8f722682687232ee640c5
matteode-91 <matt3o26@gmail.com>
2026-03-04 20:16:35 +0000

    richiama get all'interno di un'altra get

---
+++ src/main/java/it/scuola/demo/Controller.java
@@ -0,0 +1,21 @@
+package it.scuola.demo;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {    //Classe Controller
+
+    @GetMapping("/api/GET")    //Percorso della GET
+    public Map<String, Object> hello() {   //Funzione di tipo stringa
+        return Map.of(        //restituisce il messaggio sottostante
+                "message", "Hello dalla API Spring!", //messaggio che viene restituito sul browser/postman o curl
+                "ok", true
+        );
+    }
+
+   @GetMapping("/richiamo/funzione")
+   public Map<String, Object> ciao() {
+   return hello();
+   }
+}
